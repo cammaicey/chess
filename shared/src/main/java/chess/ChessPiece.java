9c93237f32b71f5c08b719654da4cd41e4ddc099
cammaicey <chelliwell.13@gmail.com>
2024-09-16 15:45:37 -0600

    bishop added but returning incorrect

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,12 +53,44 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         PieceType pType = getPieceType(); //gets piece type
         ChessGame.TeamColor tColor = getTeamColor(); //gets piece color
-        //make temporary postion
-        for (int col = myPosition.getColumn(); col <= myPosition.getColumn() + 1; col++) {
-            for (int row = myPosition.getRow(); row <= myPosition.getRow() + 1; row++) {
+        ChessPosition pos;
+        ChessMove move;
+        ArrayList<ChessMove> moves = new ArrayList<>(); //store valid moves
+        for (int col = 1; col < 9; col++) {
+            for (int row = 1; row < 9; row++) {
+                int tcol = col;
+                int trow = row;
                 if (pType == PieceType.KING) {}
                 else if (pType == PieceType.QUEEN) {}
-                else if (pType == PieceType.BISHOP) {}
+                else if (pType == PieceType.BISHOP) {
+                    //check which diagonal
+                    if (col != myPosition.getColumn() && row != myPosition.getRow()) { //can't move in the row and column
+                        while (tcol < 9 && tcol > 0 && trow > 0 && trow < 9) { //stay in bounds
+                            if (tcol == myPosition.getColumn() && trow == myPosition.getRow()) { // the move is valid
+                                pos = new ChessPosition(row, col);
+                                move = new ChessMove(myPosition, pos, getPieceType());
+                                moves.add(move);
+                                break;
+                            }
+                            else if (col < myPosition.getColumn() && row < myPosition.getRow()) {
+                                tcol++;
+                                trow++;
+                            } // column and row less than position
+                            else if (col < myPosition.getColumn() && row > myPosition.getRow()) {
+                                tcol++;
+                                trow--;
+                            } //column less than position, row greater than position
+                            else if (col > myPosition.getColumn() && row > myPosition.getRow()) {
+                                tcol--;
+                                trow--;
+                            } //column and row greater than position
+                            else if (col > myPosition.getColumn() && row < myPosition.getRow()) {
+                                tcol--;
+                                trow++;
+                            } //column greater than position, row less tan position
+                        }
+                    }
+                }
                 else if (pType == PieceType.KNIGHT) {}
                 else if (pType == PieceType.ROOK) {}
                 else if (pType == PieceType.PAWN) {}
@@ -66,6 +98,6 @@ public class ChessPiece {
         }
         //if same team is blocking don't continue to or past it
         //if different team is blocking capture and take their place
-        return new ArrayList<>();
+        return moves;
     }
 }
