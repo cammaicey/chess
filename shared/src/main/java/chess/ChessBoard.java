4216c8646ce7091e27070be7ba0381895075d74f
cammaicey <chelliwell.13@gmail.com>
2024-09-19 19:34:14 -0600

    chess board passing

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -3,6 +3,11 @@ package chess;
 import java.util.Arrays;
 import java.util.Objects;
 
+import static chess.ChessGame.TeamColor.BLACK;
+import static chess.ChessGame.TeamColor.WHITE;
+import static chess.ChessPiece.PieceType.*;
+
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -59,6 +64,33 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = null;
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new ChessPiece(WHITE, PAWN);
+            board[6][i] = new ChessPiece(BLACK, PAWN);
+        }
+
+        board[0][0] = new ChessPiece(WHITE, ROOK);
+        board[0][7] = new ChessPiece(WHITE, ROOK);
+        board[0][1] = new ChessPiece(WHITE, KNIGHT);
+        board[0][6] = new ChessPiece(WHITE, KNIGHT);
+        board[0][2] = new ChessPiece(WHITE, BISHOP);
+        board[0][5] = new ChessPiece(WHITE, BISHOP);
+        board[0][3] = new ChessPiece(WHITE, QUEEN);
+        board[0][4] = new ChessPiece(WHITE, KING);
+
+        board[7][0] = new ChessPiece(BLACK, ROOK);
+        board[7][7] = new ChessPiece(BLACK, ROOK);
+        board[7][1] = new ChessPiece(BLACK, KNIGHT);
+        board[7][6] = new ChessPiece(BLACK, KNIGHT);
+        board[7][2] = new ChessPiece(BLACK, BISHOP);
+        board[7][5] = new ChessPiece(BLACK, BISHOP);
+        board[7][3] = new ChessPiece(BLACK, QUEEN);
+        board[7][4] = new ChessPiece(BLACK, KING);
     }
 }
