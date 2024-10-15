042f4086db8d657b6a12d9051ce437b00ddde06c
cammaicey <chelliwell.13@gmail.com>
2024-10-14 18:00:29 -0600

    base game service

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.GameData;
+
+import java.util.Collection;
+
 public class GameService {
+    public GameData getGame() {
+        return null;
+    }
+    public Collection<GameData> listGames() {
+        return java.util.List.of();
+    }
+    public void updateGame(GameData game) {}
+    public GameData clear(){
+        return null;
+    }
 }
