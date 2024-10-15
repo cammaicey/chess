8ef2b6ad3ef064310b2b9d7b0188428b46745386
cammaicey <chelliwell.13@gmail.com>
2024-10-15 17:04:51 -0600

    followed PetShop and changed init for Server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,22 @@
 import chess.*;
 import com.google.gson.Gson;
+import dataaccess.*;
 import server.Server;
+import service.GameService;
+import service.UserService;
 import spark.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+
+        var userService = new UserService(userDAO, authDAO);
+        var gameService = new GameService(gameDAO, authDAO);
+
+        Server server = new Server(userService, gameService);
         server.run(8080);
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,17 @@
 package server;
 
+import service.GameService;
+import service.UserService;
 import spark.*;
 
 public class Server {
+    private final UserService userService;
+    private final GameService gameService;
+
+    public Server(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
