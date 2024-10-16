3bb2f8b506c335741f0a4a4a1f723280224c1214
cammaicey <chelliwell.13@gmail.com>
2024-10-16 16:22:49 -0600

    initial implementation

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,18 +1,29 @@
 package dataaccess;
 
 import model.UserData;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class MemoryUserDAO implements UserDAO {
-    final private HashMap<String, UserData> users = new HashMap<>();
+    final private HashSet<UserData> users = new HashSet<>();
 
     public void createUser(UserData user) {
-
+        if (getUser(user.username()) == null) { //user doesn't exist
+            users.add(user);
+        }
     }
 
     public UserData getUser(String username) {
+        for (UserData user : users) {
+            if (user.username().equals(username)) {
+                return user;
+            }
+        }
         return null;
     }
 
-    public void deleteAllUsers() {}
+    public void deleteAllUsers() {
+        for (UserData user : users) {
+            users.remove(user);
+        }
+    }
 }
