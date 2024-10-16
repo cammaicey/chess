4635fb76ad012041fdaac68342faf76aa852688e
cammaicey <chelliwell.13@gmail.com>
2024-10-16 17:05:57 -0600

    base for user handlers

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,4 +1,14 @@
 package server;
 
+import exception.ResponseException;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.api.Response;
+
 public class UserHandler {
+
+    public Object register(Request req, Response res) throws ResponseException {}
+
+    public Object login(Request req, Response res) throws ResponseException {}
+
+    public Object logout(Request req, Response res) throws ResponseException {}
 }
