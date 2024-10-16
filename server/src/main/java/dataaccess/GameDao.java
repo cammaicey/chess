0928b542b018cad5ce500acde60f4a8a820a4ae7
cammaicey <chelliwell.13@gmail.com>
2024-10-16 16:43:01 -0600

    adjusted create game return type

--- server/src/main/java/dataaccess/GameDao.java
+++ server/src/main/java/dataaccess/GameDao.java
@@ -5,7 +5,7 @@ import model.GameData;
 import java.util.Collection;
 
 public interface GameDAO {
-    GameData createGame(String gameName) throws DataAccessException;
+    void createGame(String gameName) throws DataAccessException;
 
     GameData getGame(int gameId) throws DataAccessException;
 
