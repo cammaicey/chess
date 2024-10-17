0c50f82f068b9080adc174bdc943b352a120634a
cammaicey <chelliwell.13@gmail.com>
2024-10-16 19:55:09 -0600

    base implementation of clearAllUsers and register

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,14 +1,17 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
+import exception.ResponseException;
+
+import java.util.UUID;
 
 public class UserService {
 
     private final UserDAO userDAO;
-
     private final AuthDAO authDAO;
 
     public UserService(UserDAO userDAO, AuthDAO authDAO) {
@@ -16,17 +19,23 @@ public class UserService {
         this.authDAO = authDAO;
     }
 
-    public AuthData register(UserData user) {
-        /*
+    public AuthData register(UserData user) throws ResponseException {
+        try {
+            userDAO.createUser(user);
+        } catch (DataAccessException e) {
+            throw new ResponseException(403, e.getMessage());
+        }
+        String token = UUID.randomUUID().toString();
+        String uName = user.username();
+        AuthData authData = new AuthData(token, uName);
+        return authData;
+    }
+    public AuthData login(UserData user) throws ResponseException {
+        return null;
+    }
+    public void logout(AuthData auth) throws ResponseException {}
 
-         */
-        return null;
-    }
-    public AuthData login(UserData user) {
-        return null;
-    }
-    public void logout(AuthData auth) {}
-    public UserData clear() {
-        return null;
+    public void clearUsers() throws ResponseException, DataAccessException {
+        userDAO.deleteAllUsers();
     }
 }
