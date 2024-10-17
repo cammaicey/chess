42fcea255d43a2352cc57085d796161a7d82fb12
cammaicey <chelliwell.13@gmail.com>
2024-10-16 19:35:43 -0600

    base for user handler + 1st implementation of register

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,14 +1,26 @@
 package server;
 
 import exception.ResponseException;
-import org.eclipse.jetty.client.api.Request;
-import org.eclipse.jetty.client.api.Response;
+import service.UserService;
+import model.UserData;
+import spark.*;
+import com.google.gson.Gson;
 
 public class UserHandler {
+    UserService userService;
 
-    public Object register(Request req, Response res) throws ResponseException {}
+    public Object register(Request req, Response res) throws ResponseException {
+        Gson gson = new Gson();
+        var user = gson.fromJson(req.body(), UserData.class);
+        userService.register(user);
+        return new Gson().toJson(user);
+    }
 
-    public Object login(Request req, Response res) throws ResponseException {}
+    public Object login(Request req, Response res) throws ResponseException {
+        return null;
+    }
 
-    public Object logout(Request req, Response res) throws ResponseException {}
+    public Object logout(Request req, Response res) throws ResponseException {
+        return null;
+    }
 }
