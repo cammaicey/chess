aa831f3d17058300bea72bdf616fd5df592581f6
cammaicey <chelliwell.13@gmail.com>
2024-10-20 16:01:46 -0600

    base empty methods

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -1,6 +1,8 @@
 package server;
 
 import service.GameService;
+import spark.Request;
+import spark.Response;
 
 public class GameHandler {
 
@@ -8,5 +10,17 @@ public class GameHandler {
 
     public GameHandler(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    public Object listgames(Request req, Response res) {
+        return null;
+    }
+
+    public Object creategame(Request req, Response res) {
+        return null;
+    }
+
+    public Object joingame(Request req, Response res) {
+        return null;
     }
 }
