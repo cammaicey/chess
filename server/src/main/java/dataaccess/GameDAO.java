23397fb29a56926c22dee92d1351651f4faef7dd
cammaicey <chelliwell.13@gmail.com>
2024-10-20 19:20:03 -0600

    adjust create game

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -5,7 +5,7 @@ import model.GameData;
 import java.util.Collection;
 
 public interface GameDAO {
-    void createGame(GameData game) throws DataAccessException;
+    int createGame(String gameName) throws DataAccessException;
 
     GameData getGame(int gameId) throws DataAccessException;
 
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,15 +1,31 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Random;
+import java.util.UUID;
 
 public class MemoryGameDAO implements GameDAO {
     final private HashSet<GameData> games = new HashSet<>();
 
-    public void createGame(GameData game) {
+    public int createGame(String gameName) {
+        int min = 1;
+        int max = 9999;
+        int id = generateRandomNumber(min, max);
+        while (getGame(id) != null) {
+            id = generateRandomNumber(min, max);
+        }
+        GameData game = new GameData(id, null, null, gameName, new ChessGame());
         games.add(game);
+        return id;
+    }
+
+    public static int generateRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min; // Generates a random number between min and max
     }
 
     public GameData getGame(int gameId) {
