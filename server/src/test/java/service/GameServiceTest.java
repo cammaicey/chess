5e7e2cd1aed0cb349856248c4a38dffbe102e7f3
cammaicey <chelliwell.13@gmail.com>
2024-10-22 16:16:06 -0600

    list game + create game tests

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,4 +1,122 @@
 package service;
 
+import dataaccess.*;
+import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
+import model.JoinData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.UUID;
+
 public class GameServiceTest {
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private GameService gameService;
+
+    @BeforeEach
+    public void setUp() {
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        gameService = new GameService(gameDAO, authDAO);
+    }
+
+    //list success
+    @Test
+    public void testListGames() {
+        String auth = UUID.randomUUID().toString();
+        try {
+            gameDAO.createGame("game1");
+            gameDAO.createGame("game2");
+            gameDAO.createGame("game3");
+            authDAO.createAuth(new AuthData(auth, "user1"));
+            gameService.listgames(auth);
+        } catch (ResponseException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertDoesNotThrow(() -> gameService.listgames(auth));
+    }
+
+    //list fail
+    @Test
+    public void testListGamesFail() {
+        String auth = UUID.randomUUID().toString();
+        try {
+            gameDAO.createGame("game1");
+            gameDAO.createGame("game2");
+            gameDAO.createGame("game3");
+            authDAO.createAuth(new AuthData(auth, "user1"));
+            gameService.listgames(auth);
+        } catch (ResponseException | DataAccessException e) {
+            Assertions.assertThrows(ResponseException.class, () -> gameService.listgames(auth));
+        }
+    }
+
+    //create success
+    @Test
+    public void testCreateGame() {
+        String auth = UUID.randomUUID().toString();
+        String gameName = "game1";
+        try {
+            authDAO.createAuth(new AuthData(auth, "user1"));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertDoesNotThrow(() -> gameService.creategame(auth, gameName));
+    }
+
+    //creat fail
+    @Test
+    public void testCreateGameFail() {
+        String auth = UUID.randomUUID().toString();
+        String gameName = "game!";
+        Assertions.assertThrows(ResponseException.class, () -> gameService.creategame(auth, gameName));
+    }
+
+    //join success
+    @Test
+    public void testJoinGame() {
+        String auth = UUID.randomUUID().toString();
+        int id;
+        JoinData join = null;
+        GameData game;
+        try {
+            authDAO.createAuth(new AuthData(auth, "user1"));
+            gameDAO.createGame("gameygame");
+            Collection<GameData> games = gameDAO.listGames();
+            for (GameData gameData : games) {
+                if (gameData.gameName().equals("gameygame")) {
+                    id = gameData.gameID();
+                    join = new JoinData("BLACK", id);
+                    break;
+                }
+            }
+            assert join != null;
+            gameService.joingame(auth, join);
+        } catch (ResponseException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        JoinData finalJoin = join;
+        Assertions.assertDoesNotThrow(() -> gameService.joingame(auth, finalJoin));
+    }
+
+    //join fail
+    @Test
+    public void testJoinGameFail() {
+        String auth = UUID.randomUUID().toString();
+        JoinData join = null;
+        GameData game;
+        try {
+            authDAO.createAuth(new AuthData(auth, "user1"));
+            gameDAO.createGame("gameygame");
+            join = new JoinData("BLACK", 1);
+            gameService.joingame(auth, join);
+        } catch (ResponseException | DataAccessException e) {
+            JoinData finalJoin = join;
+            Assertions.assertThrows(ResponseException.class, () -> gameService.joingame(auth, finalJoin));
+        }
+    }
 }
