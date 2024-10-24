6834f47c7239c8eb85cbd92b148136757e8c0bae
cammaicey <chelliwell.13@gmail.com>
2024-10-24 17:44:50 -0600

    game sql base

--- server/src/main/java/dataaccess/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -1,4 +1,66 @@
 package dataaccess;
 
-public class MySQLGameDAO {
+import exception.ResponseException;
+import model.GameData;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class MySQLGameDAO implements GameDAO {
+
+    public MySQLGameDAO() throws ResponseException, SQLException, DataAccessException {
+        configDatabase();
+    }
+
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameId) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(String playerColor, int gameID, String auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public void deleteAllGames() throws DataAccessException {
+
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS game (
+             'gameID' INT NOT NULL,
+             'whiteUsername' varchar(255),
+             'blackUsername' varchar(255),
+             'gameName' varchar(255),
+             chessGame TEXT,
+             PRIMARY KEY ('gameID')
+            )
+            """
+    };
+
+    private void configDatabase() throws ResponseException, DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
