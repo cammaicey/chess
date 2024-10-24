e6d3ed4d477e4517572146d22112442d0250bd60
cammaicey <chelliwell.13@gmail.com>
2024-10-24 16:47:28 -0600

    added executeUpdate

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -5,6 +5,10 @@ import com.google.gson.Gson;
 import model.UserData;
 
 import java.sql.SQLException;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySQLUserDAO implements UserDAO {
 
@@ -27,8 +31,28 @@ public class MySQLUserDAO implements UserDAO {
 
     }
 
-    private int executeUpdate(String statement, Object...params) throws ResponseException {
-        return 0;
+    private int executeUpdate(String statement, Object...params) throws ResponseException, DataAccessException, SQLException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof UserData p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            } catch (SQLException e) {
+                throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+            }
+        }
     }
 
     private final String[] createStatements = {
