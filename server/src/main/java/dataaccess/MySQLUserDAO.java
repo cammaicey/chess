4f36bc206f4b75cf6c7bf9cef02310ea634208d6
cammaicey <chelliwell.13@gmail.com>
2024-10-24 16:35:48 -0600

    empty methods + base idea of database config

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,4 +1,52 @@
 package dataaccess;
 
-public class MySQLUserDAO {
+import exception.ResponseException;
+import model.UserData;
+
+import java.sql.SQLException;
+
+public class MySQLUserDAO implements UserDAO {
+
+    public MySQLUserDAO() throws ResponseException, SQLException, DataAccessException {
+        configDatabase();
+    }
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAllUsers() throws DataAccessException {
+
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+             username varchar(255) NOT NULL,
+             password varchar(255) NOT NULL,
+             email varchar(255),
+             PRIMARY KEY ('username'),
+            )
+            """
+    };
+
+    private void configDatabase() throws ResponseException, DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
