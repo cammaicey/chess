23d41bc458e8317eefa92a28bdeb9d50d0d94c40
cammaicey <chelliwell.13@gmail.com>
2024-10-28 18:34:18 -0600

    logout success

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import exception.ResponseException;
 import model.AuthData;
+import model.UserData;
 
 import java.sql.SQLException;
 
@@ -12,13 +13,26 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     @Override
-    public void createAuth(AuthData authData) throws DataAccessException {
-
+    public void createAuth(AuthData authData) throws DataAccessException, ResponseException, SQLException {
+        var statement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        executeUpdate(statement, authData.authToken(), authData.username());
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        var statement = "SELECT authToken, username FROM auth WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    rs.next();
+                    String username = rs.getString("username");
+                    return new AuthData(authToken, username);
+                }
+            }
+        } catch (SQLException e) {
+            return null;
+        }
     }
 
     @Override
@@ -32,8 +46,24 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     @Override
-    public void deleteAllAuths() throws DataAccessException {
+    public void deleteAllAuths() throws DataAccessException, ResponseException, SQLException {
+        var statement = "TRUNCATE auth";
+        executeUpdate(statement);
+    }
 
+    private void executeUpdate(String statement, Object...params) throws ResponseException, DataAccessException, SQLException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                String sql = statement.trim();
+                if (sql.toUpperCase().startsWith("INSERT")) {
+                    ps.setString(1, params[0].toString());
+                    ps.setString(2, params[1].toString());
+                }
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 
     private final String[] createStatements = {
