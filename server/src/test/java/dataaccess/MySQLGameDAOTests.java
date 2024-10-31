baca7ac3dd2d0b036c5ceaef23c0a0633ac7c46b
cammaicey <chelliwell.13@gmail.com>
2024-10-30 18:38:54 -0600

    create game positive and negative

--- server/src/test/java/dataaccess/MySQLGameDAOTests.java
+++ server/src/test/java/dataaccess/MySQLGameDAOTests.java
@@ -1,4 +1,59 @@
 package dataaccess;
 
+import chess.ChessGame;
+import exception.ResponseException;
+import model.GameData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class MySQLGameDAOTests {
+    GameDAO gameDAO;
+    int gameID;
+
+    @BeforeEach
+    void setUp() throws DataAccessException, SQLException, ResponseException {
+        DatabaseManager.createDatabase();
+        gameDAO = new MySQLGameDAO();
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("TRUNCATE game")) {
+                statement.executeUpdate();
+            }
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException, DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.prepareStatement("TRUNCATE game")) {
+                statement.executeUpdate();
+            }
+        }
+    }
+
+    //create
+    @Test
+    public void testCreatGame() {
+        Assertions.assertDoesNotThrow(() -> {gameDAO.createGame("new game");});
+    }
+
+    @Test
+    public void testCreatGameFail() {
+        try {
+            gameDAO.createGame("game name");
+        } catch (ResponseException | DataAccessException | SQLException e) {
+            Assertions.assertThrows(ResponseException.class, () -> {gameDAO.createGame("");});
+        }
+    }
+
+    //list
+
+    //join
+
+    //clear
+    @Test
+    public void testClearGames() {}
 }
