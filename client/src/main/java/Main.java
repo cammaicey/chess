8a006f56b65811a3f8f9e9eaffa44d0a84e055fb
cammaicey <chelliwell.13@gmail.com>
2024-11-09 16:49:15 -0700

    interactive menu
    No proper functionality yet

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,15 @@
 import chess.*;
 import ui.DrawBoard;
+import ui.REPL;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
-        DrawBoard drawBoard = new DrawBoard(new ChessGame());
-        drawBoard.drawBoard();
+        //DrawBoard drawBoard = new DrawBoard(new ChessGame());
+        //drawBoard.drawBoard();
+        REPL repl = new REPL();
+        repl.run();
     }
 }
---
+++ client/src/main/java/ui/REPL.java
@@ -0,0 +1,118 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class REPL {
+
+    private final ChessClient client;
+
+    public REPL() {
+        client = new ChessClient();
+    }
+
+    public void run() {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(ERASE_SCREEN);
+
+        boolean loggedIn = false;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!loggedIn) {
+            prelogin(out);
+
+            String line = scanner.nextLine();
+
+            if (Objects.equals(line, "1")) {
+                menuRegister(out, scanner);
+                loggedIn = true;
+            }
+            else if (Objects.equals(line, "2")) {
+                menuLogin(out, scanner);
+                loggedIn = true;
+            }
+            else if (Objects.equals(line, "3")) {
+                out.println("Press 1 to register as a new user.");
+                out.println("Press 2 to login as an existing user.");
+                out.println("Press 3 to exit the program.\n");
+            }
+            else if (Objects.equals(line, "4")) {
+                out.println("Goodbye!");
+                out.close();
+                System.exit(0);
+            }
+            else {
+                out.println("Invalid selection.");
+            }
+        }
+        while (loggedIn) {
+            postlogin(out);
+            String line = scanner.nextLine();
+            if (Objects.equals(line, "1")) {
+                loggedIn = false;
+            }
+            else if (Objects.equals(line, "2")) {
+                out.println("Please enter a name for your game.");
+                scanner.nextLine();
+            }
+            else if (Objects.equals(line, "3")) {
+                //numbered list (doesn't correspond to game id)
+                //game name + player names
+            }
+            else if (Objects.equals(line, "4")) {
+                out.println("Please enter the number of the game you wish to join.");
+                scanner.nextLine();
+                out.println("What color do you wish to be?");
+                scanner.nextLine();
+                //join based on input
+            }
+            else if (Objects.equals(line, "5")) {
+                out.println("Please enter the number of the game you wish to observe.");
+                scanner.nextLine();
+            }
+            else {
+                out.println("Invalid selection.");
+            }
+        }
+
+    }
+
+    private void prelogin(PrintStream out) {
+        out.println("Welcome! Please type a number corresponding to the ones below.");
+        out.println("\t1. Register");
+        out.println("\t2. Login");
+        out.println("\t3. Help");
+        out.println("\t4. Quit");
+    }
+
+    private void menuRegister (PrintStream out, Scanner scanner) {
+        out.println("Please enter a username.");
+        scanner.nextLine();
+        out.println("Please enter and email address.");
+        scanner.nextLine();
+        out.println("Please enter a password.");
+        scanner.nextLine();
+    }
+
+    private void menuLogin (PrintStream out, Scanner scanner) {
+        out.println("Please enter your username.");
+        scanner.nextLine();
+        out.println("Please enter your password.");
+        scanner.nextLine();
+    }
+
+    private void postlogin(PrintStream out) {
+        out.println("Please type a number corresponding to the ones below.");
+        out.println("\t1. Logout");
+        out.println("\t2. Create Game");
+        out.println("\t3. List Games");
+        out.println("\t4. Play Game");
+        out.println("\t5. Observe Game");
+    }
+
+}
