35fd50f7ba14ac57a776e9811ae1f41e32861f17
cammaicey <chelliwell.13@gmail.com>
2024-11-13 15:24:54 -0700

    forgot to update to throw exception

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,11 @@
 import chess.*;
 import client.ServerFacade;
+import exception.ResponseException;
 import ui.DrawBoard;
 import ui.REPL;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ResponseException {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
         var serverURL = "http://localhost:8080";
