cee3a136164d03ef9cb44d59d572fce174a95b67
cammaicey <chelliwell.13@gmail.com>
2024-12-01 11:55:57 -0700

    added setting game and color adjusted load and message handler

--- client/src/main/java/client/WebSocketCommunicator.java
+++ client/src/main/java/client/WebSocketCommunicator.java
@@ -1,18 +1,26 @@
 package client;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
+import ui.DrawBoard;
+import ui.REPL;
 import websocket.messages.ServerMessage;
 
 import javax.websocket.*;
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Objects;
 
 import static ui.EscapeSequences.ERASE_LINE;
 
 public class WebSocketCommunicator extends Endpoint {
 
     Session session;
+    DrawBoard drawBoard;
+    private ChessGame game;
+    private String color;
 
     public WebSocketCommunicator(String domain) throws Exception {
         try {
@@ -39,24 +47,36 @@ public class WebSocketCommunicator extends Endpoint {
         this.session.getAsyncRemote().sendText(message);
     }
 
+    protected void setGameAndColor(ChessGame game, String color) {
+        this.game = game;
+        this.color = color;
+    }
+
     private void messageHandler(String message) {
         ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
         ServerMessage.ServerMessageType type = serverMessage.getServerMessageType();
 
-        switch (message) {
-            //case  -> notify(message);
-            //case ERROR -> error(message);
-            //case LOAD_GAME -> load(message);
+        if (Objects.equals(type.toString(), "NOTIFICATION")) {
+            notify(message);
+        }
+        else if (Objects.equals(type.toString(), "ERROR")) {
+            error(message);
+        }
+        else if (Objects.equals(type.toString(), "LOAD_GAME")) {
+            load(message);
         }
     }
 
     private void error(String message) {}
 
-    private void load(String message) {}
+    private void load(String message) {
+        ServerMessage loadGame = new Gson().fromJson(message, ServerMessage.class);
+        System.out.print(ERASE_LINE + "\r\n");
+        drawBoard = new DrawBoard(game, color);
+    }
 
     private void notify(String message) {
         System.out.print(ERASE_LINE + '\r');
-        System.out.printf("\n%s\n[IN-GAME] >>> ", message);
     }
 
     @Override
